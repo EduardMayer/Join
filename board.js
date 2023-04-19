5995ea91f52d9ed4bc74bd4910abec9065dd2bed
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-04-19 20:57:04 +0200

    function add / Style add

--- board.js
+++ board.js
@@ -1,8 +1,11 @@
+let currentElement = null;
+
+
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        file = element.getAttribute("w3-include-html"); // "includes/head_navbar.html"
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
@@ -16,3 +19,38 @@ function openAddTaskContainer(){
 
 
 }
+
+function addSubtask(){
+    let subtask = document.getElementById('subtask').value;
+    document.getElementById('subTaskDescription').innerHTML += `<div class="checkContainer"><input type="checkbox"> ${subtask}</div>`;
+    document.getElementById('subtask').value = ``;
+}
+
+
+function resetImage(box) {
+    const img = box.querySelector("img");
+    const defaultImg = img.dataset.defaultImg;
+    img.src = defaultImg;
+  }
+
+function checkpriobox(event) {
+    let element = event.target;
+    
+    if (currentElement !== null) {
+      currentElement.style.backgroundColor = "";
+      resetImage(currentElement);
+    }
+    
+    if (element.id === "urgentbox") {
+      element.style.backgroundColor = "rgb(255, 61, 0)";
+      element.querySelector("img").src = "/img/Prio-urgent-white.png"; 
+    } else if (element.id === "mediumbox") {
+      element.style.backgroundColor = "rgb(255, 168, 0)";
+      element.querySelector("img").src = "/img/Prio-medium-white.png"; 
+    } else if (element.id === "lowbox") {
+      element.style.backgroundColor = "rgb(122,226,41)";
+      element.querySelector("img").src = "/img/Prio-low-white.png"; 
+    }
+    
+    currentElement = element;
+  }
