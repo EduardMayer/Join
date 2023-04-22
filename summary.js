c6ab03c2e5eb732ed4ad96e47a0f559ffa3849dd
Daniel Schirmer <tdk19@web.de>
2023-04-23 00:55:35 +0200

    revised version

---
+++ summary.js
@@ -0,0 +1,29 @@
+function renderGreetingMassage() {
+    let user = getUser();
+    let message = getGreeting();
+    if (user == 'Guest') {
+        document.getElementById('greeting-message').innerHTML = message;
+        document.getElementById('greeting-user').innerHTML = '';
+        
+    } else {
+        document.getElementById('greeting-message').innerHTML = message + ',';
+        document.getElementById('greeting-user').innerHTML = user;
+    }
+
+}
+
+
+function getGreeting() {
+    let time = new Date();
+    time = time.getHours();
+
+    if (time >= 5 && time < 12) {
+        return 'Good morning';
+    }
+    if (time >= 12 && time < 18) {
+        return 'Good afternoon';
+    }
+    if ((time >= 18 && time < 24) || (time >= 0 && time < 5)) {
+        return 'Good evening';
+    }
+}
