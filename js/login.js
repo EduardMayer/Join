2c398d1faa4305301cf0b58b995a2cbb0b1cb388
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-04-24 13:04:06 +0200

    login function

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function login(){
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(u => u.email.value == email.value && u.password.value == password.value);
+    console.log(user);
+}
