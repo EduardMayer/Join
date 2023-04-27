7df851dde7c641a1c6253642b9a73b76ab6f597c
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-04-27 18:09:57 +0200

    fixed category and priority

--- board.js
+++ board.js
@@ -1,7 +1,8 @@
 let currentElement = null;
-let clickedId;
+let clickedId = null;
 let allTasks = [];
 let allCategory = [];
+
 
 // Header und linke Navigationsleiste wird hinzugefügt
 async function includeHTML() {
@@ -34,6 +35,37 @@ function openDropBoxCategory() {
     dropDownBox.classList.remove("dropDownBox");
     childTaskContainer.classList.remove("b-none");
   }
+}
+
+function newCategory(){
+let categoryContainer = document.getElementById("categoryContainer");
+let newCategoryContainer = document.getElementById("newCategoryContainer");
+
+categoryContainer.classList.add("d-none");
+newCategoryContainer.classList.remove("d-none");
+
+document.getElementById("newCategoryContainer").innerHTML =`<label for="Category">Category</label><div class="subtaskChildContainer" >
+<input type="text" placeholder="New category name">
+<div class="subImgContainer">
+  <img onclick="closeNewCategory()" src="/img/iconoir_cancel_black.svg">
+  <div class="searchBarLine"></div>
+  <img onclick="addNewCategory()" id="subImg" src="/img/akar-icons_check_black.svg">
+</div>
+</div>`;
+
+}
+
+function closeNewCategory(){
+  let categoryContainer = document.getElementById("categoryContainer");
+  let newCategoryContainer = document.getElementById("newCategoryContainer");
+  let dropDownBox = document.getElementById("dropDownBox");
+  let childTaskContainer = document.getElementById("category");
+
+  categoryContainer.classList.remove("d-none");
+  newCategoryContainer.classList.add("d-none");
+  dropDownBox.classList.add("d-none");
+  dropDownBox.classList.remove("dropDownBox");
+  childTaskContainer.classList.remove("b-none");
 }
 
 function openDropBoxAssigned() {
@@ -72,8 +104,8 @@ function addSubtask() {
   let subtask = document.getElementById("subtask").value;
   document.getElementById(
     "subTaskDescription"
-  ).innerHTML += `<div class="checkContainer" ><input type="checkbox"><div>${subtask}</div></div>`;
-  document.getElementById("subtask").value = ``;
+  ).innerHTML += `<div class="checkContainer" ><input type="checkbox""><div id="subBox">${subtask}</div></div>`;
+  document.getElementById("subtask").value = ``; 
 }
 
 // Die Buttons für die Priorität bekommen nach anklicken eine neue Farbe
@@ -83,27 +115,36 @@ function resetImage(box) {
   img.src = defaultImg;
 }
 
+
+
 function checkpriobox(event) {
   let element = event.target;
 
-  if (currentElement !== null) {
-    currentElement.style.backgroundColor = "";
-    resetImage(currentElement);
-  }
+  if (currentElement === element) { // Check if clicked element is the same as current element
+    element.style.backgroundColor = "";
+    resetImage(element);
+    currentElement = null;
+    clickedId = null;
+  } else {
+    if (currentElement !== null) { // Check if there is a current element
+      currentElement.style.backgroundColor = "";
+      resetImage(currentElement);
+    }
 
-  if (element.id === "urgent") {
-    element.style.backgroundColor = "rgb(255, 61, 0)";
-    element.querySelector("img").src = "/img/Prio-urgent-white.png";
-  } else if (element.id === "medium") {
-    element.style.backgroundColor = "rgb(255, 168, 0)";
-    element.querySelector("img").src = "/img/Prio-medium-white.png";
-  } else if (element.id === "low") {
-    element.style.backgroundColor = "rgb(122,226,41)";
-    element.querySelector("img").src = "/img/Prio-low-white.png";
-  }
+    if (element.id === "urgent") {
+      element.style.backgroundColor = "rgb(255, 61, 0)";
+      element.querySelector("img").src = "/img/Prio-urgent-white.png";
+    } else if (element.id === "medium") {
+      element.style.backgroundColor = "rgb(255, 168, 0)";
+      element.querySelector("img").src = "/img/Prio-medium-white.png";
+    } else if (element.id === "low") {
+      element.style.backgroundColor = "rgb(122,226,41)";
+      element.querySelector("img").src = "/img/Prio-low-white.png";
+    }
 
-  currentElement = element;
-  clickedId = event.target.id; 
+    currentElement = element;
+    clickedId = event.target.id;
+  }
 }
 
 
@@ -140,7 +181,7 @@ function createTask() {
     "priority": priority,
   };
 
-//allTasks.push(allTask);
+  allTasks.push(allTask);
   console.log(allTasks);
   console.log(subtask);
 
@@ -149,9 +190,10 @@ clearTask(title, description, date, priority);
 
 function clearTask(title, description, date,){
   
-  currentElement.style.backgroundColor = "";
-  resetImage(currentElement);
   
+  currentElement.style.backgroundColor = "";
+  
+  resetImage(currentElement);
   title.value = ``;
   description.value = ``;
   date.value = ``;
