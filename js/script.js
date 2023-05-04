7d9b9a71e28157c7a2e2170a8cd26a24f1c7c115
Stefan Horning <stefanhorning@Stefans-iMac.fritz.box>
2023-05-04 19:52:55 +0200

    login

--- js/script.js
+++ js/script.js
@@ -87,7 +87,7 @@ function renderHTML(){
                       </div>
                       <div class="login-buttons">
                         <div>
-                          <button onsubmit="return false; login();" class="login-button">Log in</button>
+                          <button type="submit" onclick="login(event)" class="login-button">Log in</button>
                         </div>
                         <div>
                           <button onclick="guestLogin()" class="guest-button">Guest Log in</button>
