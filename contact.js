bd549d5a8c38acd84e74b32b7612bf5c1e062819
Daniel-Schi <tdk19@web.de>
2023-05-06 14:57:39 +0200

    add contact

---
+++ contact.js
@@ -0,0 +1,106 @@
+
+
+function letterHTML(letter, i){
+    return /* html */ `
+        <div>
+            <h2>${letter}</h2>
+            <div id="contact-${letter}">
+                <div>
+                    <img src="contact/person..." alt="">
+                </div>
+                <div>
+                    <h3>${name[i]}</h3>
+                    <a href="#">${email[i]}</a>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function init(){
+
+}
+
+function newContactHTML(){
+    return /* html */ `
+        <div>
+            <div class="newContact-logo">
+                <div>
+                    <img src="img/logoJoin.svg" alt="">
+                </div>
+                <div>
+                    <span>Add contact</span>
+                    <span>Tasks are better with a team!</span>
+                    <div style="height:2px; width:50px; background-color:rgb(40, 172, 226);"></div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="profile picture" alt="">
+                </div>
+                <form onsubmit="createContact()">
+                    <div>
+                        <input required type="text" placeholder="Name" pattern="[a-zA-ZÄäÜüÖöß ]*" minlength="2" maxlength="30">
+                        <img src="img/people.svg" alt="">
+                    </div>
+                    <div>
+                        <input required type="email" placeholder="Email">
+                        <img src="img/emailicon.svg" alt="">
+                    </div>
+                    <div>
+                        <input required type="number" placeholder="Email" minlength="11" maxlength="16">
+                        <img src="img/Vector.svg" alt="">
+                    </div>
+                    <div>
+                        <div>
+                            <button type="button">Cancel <img src="img/cancel.svg" alt=""></button>
+                        </div>
+                        <div>
+                            <button type="submit">Create contact <img src="img/chop.svg" alt=""></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    `;
+}
+
+function editContactHTML(){
+    return /* html */ `
+        <div>
+            <img src="img/cancel.svg" alt="">
+            <div>
+                <img src="img/logoJoin.svg" alt="">
+                <span>Edit contact</span>
+                <div style="height:2px; width:50px; background-color:rgb(40, 172, 226);"></div>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="">
+                </div>
+                <form onsubmit="createContact()">
+                    <div>
+                        <input required type="text" placeholder="Name" pattern="[a-zA-ZÄäÜüÖöß ]*" minlength="2" maxlength="30">
+                        <img src="img/people.svg" alt="">
+                    </div>
+                    <div>
+                        <input required type="email" placeholder="Email">
+                        <img src="img/emailicon.svg" alt="">
+                    </div>
+                    <div>
+                        <input required type="number" placeholder="Email" minlength="11" maxlength="16">
+                        <img src="img/Vector.svg" alt="">
+                    </div>
+                    <div>
+                        <div>
+                            <button type="button">Delete </button>
+                        </div>
+                        <div>
+                            <button type="submit">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    `;
+}
