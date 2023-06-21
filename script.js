7ae2c991911e06df61dd12cb436f8a768ee901ab
Eduard Mayer <119446765+EduardMayer@users.noreply.github.com>
2023-06-21 11:02:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+let isPasswordSeen = false;
+let eyeImg = false;
+
+function init(){
+    render();
+}
+
+function render(){
+    let div = document.getElementById('loginDiv');
+    div.innerHTML = renderHTML();
+}
+
+function renderSignUp(){
+    document.getElementById('navbar').classList.add('d-none');
+    let div = document.getElementById('loginDiv');
+    div.innerHTML = SignUpHTML();
+}
+
+function renderPassword(){
+    document.getElementById('navbar').classList.add('d-none');
+    let div = document.getElementById('loginDiv');
+    div.innerHTML = forgotMyPasswordHTML();
+}
+
+function seePassword(){
+    let input = document.getElementById('password');
+    let img = document.getElementById('password-img');
+    if(!isPasswordSeen){
+        input.type = 'text';
+        isPasswordSeen = true;
+        img.src = 'img/seePassword.svg';
+    }else{
+        input.type = 'password';
+        isPasswordSeen = false;
+        img.src = 'img/seeNotPassword.svg';
+    }
+}
+
+function checkInput(){
+    let input = document.getElementById('password');
+    let img = document.getElementById('password-img');
+    if(input.value == ''){
+        input.type = 'password';
+        img.src = 'img/passwordicon.svg';
+    }else{
+        img.src = 'img/seeNotPassword.svg';
+    }
+}
+
+function back(){
+    document.getElementById('navbar').classList.remove('d-none');
+    render();
+}
+
+function renderHTML(){
+    return`
+        <div>
+            <h1>Log in</h1>
+            <div class="overH1-line">
+                <div class="h1-line"></div>
+            </div>
+        </div>
+        <div class="login-value">
+            <form>
+                <div class="relativ-for-icon">
+                    <input required placeholder="Email" type="email">
+                    <img src="img/emailicon.svg" alt="">
+                </div>
+                <div class="relativ-for-icon">
+                    <input required onkeyup="checkInput()" id="password" placeholder="Password" type="password">
+                    <img id="password-img" onclick="seePassword()" src="img/passwordicon.svg" alt="">
+                </div>
+            </form>
+        </div>
+        <div class="password-options">
+            <div class="checkbox">
+                <input type="checkbox">
+                <span>Remember me</span>
+            </div>
+            <div class="forgot">
+                <a onclick="renderPassword()">Forgot my password</a>
+            </div>
+        </div>
+        <div class="login-buttons">
+            <div>
+                <button class="login-button">Log in</button>
+            </div>
+            <div>
+                <button class="guest-button">Guest Log in</button>
+            </div>
+        </div>
+    </div>
+`;
+}
+
+function SignUpHTML(){
+    return `
+    <div class="back">
+    <img onclick="back()" src="img/blackarrowicon.svg" alt="">
+</div>
+<div>
+    <h1>Sign up</h1>
+    <div class="overH1-line">
+        <div class="h1-line"></div>
+    </div>
+</div>
+<div class="login-value">
+    <form>
+        <div class="relativ-for-icon">
+            <input required id="inputName" placeholder="name" type="text" minlength="2">
+            <img src="img/people.svg" alt="">
+        </div>
+        <div class="relativ-for-icon">
+            <input required id="inputEmail" placeholder="Email" type="email">
+            <img src="img/emailicon.svg" alt="">
+        </div>
+        <div class="relativ-for-icon">
+            <input required onkeyup="checkInput()" id="password" placeholder="Password" type="password">
+            <img id="password-img" onclick="seePassword()" src="img/passwordicon.svg" alt="">
+        </div>
+    </form>
+</div>
+<div class="login-buttons-sign-up">
+    <div>
+        <button class="login-button">Sign up</button>
+    </div>
+</div>
+    `;
+}
+
+function forgotMyPasswordHTML(){
+    return `
+    
+    <div class="back">
+        <img onclick="back()" src="img/blackarrowicon.svg" alt="">
+    </div>
+    <div>
+        <h1>I forgot my password</h1>
+        <div class="overH1-line">
+            <div class="h1-line"></div>
+        </div>
+    </div>
+    <span class="Password-text">Don't worry! We will send you an email with the instructions to <br> reset your password.</span>
+    <div class="login-value">
+        <form>
+            <div class="relativ-for-icon">
+                <input required placeholder="Email" type="email">
+                <img src="img/emailicon.svg" alt="">
+            </div>
+        </form>
+    </div>
+    <div class="login-buttons-sign-up">
+        <div>
+            <button style="width: 200px; margin-top: 20px;" class="login-button">Send me the Email</button>
+        </div>
+    </div>
+</div>
+
+    `;
+}
